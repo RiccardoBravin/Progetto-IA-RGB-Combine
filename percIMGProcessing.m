6bfc347a5702e321bf1184dbdc8b41d3e683a073
Riccardo Bravin <riccardo.bravin.1@studenti.unipd.it>
2022-04-16 15:34:49 +0200

    Rinominato file e gitignore

--- percIMGProcessing.m
+++ percIMGProcessing.m
@@ -6,7 +6,7 @@
 %        |->   .
 %        |-> NCSU-CUB_Foram_Images_Others    
 
-path = {'NCSU-CUB_Foram_Images_G-bulloides','NCSU-CUB_Foram_Images_G-ruber','NCSU-CUB_Foram_Images_G-sacculifer','NCSU-CUB_Foram_Images_N-dutertrei','NCSU-CUB_Foram_Images_N-incompta','NCSU-CUB_Foram_Images_N-pachyderma','NCSU-CUB_Foram_Images_Others'};
+path = {'G. Bulloides','G. Ruber','G. Sacculifer','N. Dutertrei','N. Incompta','N. Pachyderma','Others'};
 outF = 'percentileIMG';
 
 %create output folder

95b745fe55be97ea1c267646f5373422b196b1bb
RiccardoBravin <riccardo.bravin.1@studenti.unipd.it>
2022-05-31 16:01:26 +0200

    Fixed correctly color wheel group

--- Processing/colorWheelGroupProcessing.m
+++ Processing/colorWheelGroupProcessing.m
@@ -1,6 +1,6 @@
 clear
 
-groupingBy = 1;
+groupingBy = 5;
 pw = 2;
 
 path = {'G_Bulloides','G_Ruber','G_Sacculifer','N_Dutertrei','N_Incompta','N_Pachyderma','Others'};
@@ -36,39 +36,32 @@ for K = 1 : length(path)
 
         RGB = zeros(imgR,imgC,3);
 
-        for d = 1:16
-            for ch = 1:3
-                for gc = 1:groupingBy
-                    disp((ch-1)*groupingBy+gc);
-                    if ((ch-1)*groupingBy+gc) <=16
-                        RGB(:,:,ch) = RGB(:,:,ch) + px(:,:,(ch-1)*groupingBy+gc).^pw;
+        d = 1;
+        while d <= 16
+            for chan = 1:3
+                for i = 1:groupingBy
+                    if(d <= 16)
+                        RGB(:,:,chan) = RGB(:,:,chan) + px(:,:,d).^pw;
+                        d = d + 1;
                     end
                 end
             end
         end
-        
-        d = 1
-        while d <= 16
-            for i = 1:groupingBy
-                RGB(:,:,1)
-            end
-
-        end
 
 
         RGB = uint8(rescale(RGB, 0, 255));
-        
+
 
         RGB2 = RGB;
         RGB2 = imlocalbrighten(RGB2, 0.5, 'AlphaBlend',true);
         RGB2 = imreducehaze(RGB2,0.9,'method','approxdcp');
-        
-        
-%         nome = strcat(outF,'/',path{K},'/',char(imB.Labels(I-1)),'.png');
-%         imwrite(RGB,nome);
-% 
-%         nome = strcat(outFPP,'/',path{K},'/',char(imB.Labels(I-1)),'.png');
-%         imwrite(RGB2,nome);
+
+
+        %         nome = strcat(outF,'/',path{K},'/',char(imB.Labels(I-1)),'.png');
+        %         imwrite(RGB,nome);
+        %
+        %         nome = strcat(outFPP,'/',path{K},'/',char(imB.Labels(I-1)),'.png');
+        %         imwrite(RGB2,nome);
 
         montage({RGB,RGB(:,:,1),RGB(:,:,2),RGB(:,:,3)}); pause(1);
         montage({RGB2,RGB2(:,:,1),RGB2(:,:,2),RGB2(:,:,3)}); pause(1);
